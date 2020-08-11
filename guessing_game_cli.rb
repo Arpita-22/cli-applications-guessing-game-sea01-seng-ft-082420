b3a44dbaa158eb4dedab1317aff5ea0596ca446b
Arpita Dutta <duttaarpita22@gmail.com>
2020-08-11 23:36:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,5 +4,9 @@ def run_guessing_game
   input = gets.chomp
   if input == rand_no
     puts "You guessed the correct number!"
+  elsif input == "exit"
+  puts "Goodbye!"
+else
+  puts "Sorry! The computer guessed #{rand_no}"
   end
 end
